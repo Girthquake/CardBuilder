0a1f56909ba22a96857ee6855c7bd368e3fcd86b
bryan beasley <wbbeasleyii@gmail.com>
2021-05-27 13:11:06 -0500

    Initial Photo injest code

--- ImageInjest.py
+++ ImageInjest.py
@@ -1,18 +1,97 @@
 from tkinter import *
-from PIL import Image, ImageTk
+import tkinter as tk
+import PIL
+from PIL import ImageTk, Image
+import glob
+import os
 
 # --- functions ---
 
-def on_click(widget):
-    print('clicked')
-    widget['image'] = img2
+i = 0
+img=0
+imagelist=[]
+imagelistname=[]
+path = 'injest/'
+equipnumberentry = ""
+imagelableentry = "" 
+imageorderentry = ""
 
-# --- main ---
-root = tk()
-root.geometry('1000x1000')
-canvas = Canvas(root,width=999,height=999)
-canvas.pack()
-pilImage = Image.open("ball.jpg")
-image = ImageTk.PhotoImage(pilImage)
-imagesprite = canvas.create_image(400,400,image=image)
+
+
+def text_mod():
+    global i, btn2, imagelist, image, imagelistname          # btn can be omitted but not sure if should be
+    btn2['text'] = imagelistname[i]    # the global object that is modified
+    photo = load_images()
+    i = (i + 1) % len(imagelistname)  # another global object that gets modified
+    item4 = canvas.create_image(209, 164, image=photo)
+    root.mainloop()
+
+def load_images():
+    global i, btn2, imagelist, image,imagelistname
+    image = Image.open(imagelistname[i])
+    basewidth = 900
+    #wpercent = (basewidth / float(image.size[0]))
+    #hsize = int((float(image.size[1]) * float(wpercent)))
+    image = image.resize((418,328), PIL.Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(image)
+    return photo
+
+def resize_Image(simage):
+    im = Image.open(simage)
+    if im.width != 318 and im.height != 228:
+        resized_im = im.resize((318,228))
+        resized_im.save(simage)
+
+def injest_image():
+    global imagelableentry, imageorderentry, imagelistname, equipnumberentry
+    im = Image.open(imagelistname[i])
+    if im.width != 318 and im.height != 228:
+        im = im.resize((318,228))
+        s1=str(equipnumberentry.get())
+        s2=str(imagelableentry.get())
+        s3=str(imageorderentry.get())
+        if not os.path.exists("images/"+s1):
+            os.makedirs("images/"+s1)
+
+        im.save("images/"+s1+"/"+s2+" "+str(s3)+".jpg")
+        imagelistname.remove(imagelistname[i])
+
+root=Tk()
+
+
+for filename in glob.glob(path+'/*.jpg'): #assuming gif
+    im=Image.open(filename)
+    imagelist.append(im)
+    imagelistname.append(filename)
+
+canvas=Canvas(root, height=330, width=1000)
+
+eqnlable = tk.Label(root, text='Equipment Number:')
+eqnlable.config(font=('helvetica', 10))
+canvas.create_window(550, 140, window=eqnlable)
+equipnumberentry = tk.Entry (root) 
+canvas.create_window(680, 140, window=equipnumberentry)
+
+imglable = tk.Label(root, text='Image lable:')
+imglable.config(font=('helvetica', 10))
+canvas.create_window(550, 160, window=imglable)
+imagelableentry = tk.Entry (root) 
+canvas.create_window(680, 160, window=imagelableentry)
+
+imgorder = tk.Label(root, text='Image order ( 1-6 ):')
+imgorder.config(font=('helvetica', 10))
+canvas.create_window(550, 180, window=imgorder)
+imageorderentry = tk.Entry (root) 
+canvas.create_window(680, 180, window=imageorderentry)
+
+btn = tk.Button(root, text="Injest Image", height=2, width = 20)
+btn['command'] = injest_image
+
+btn2 = tk.Button(root, text="Cycle Images",height=2)
+btn2['command'] = text_mod
+
+btn.pack(fill='both', expand=True)
+btn.place(x=600, y=300)
+btn2.pack(fill='both', expand=True)
+canvas.pack(side = TOP, expand=True, fill=BOTH)
 root.mainloop()
